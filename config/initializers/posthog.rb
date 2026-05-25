3c6e48f96e2f2bc212b33fbaeb623d4782242005
AkaMandakeru <jsilva-contractor@taskrabbit.com>
2026-05-25 20:46:12 -0300

    removed metics from dev env

--- config/initializers/posthog.rb
+++ config/initializers/posthog.rb
@@ -2,7 +2,7 @@
 
 require "posthog"
 
-POSTHOG_CLIENT = if ENV["POSTHOG_API_KEY"].present?
+POSTHOG_CLIENT = if ENV["POSTHOG_API_KEY"].present? && (Rails.env.production? || ENV["POSTHOG_ENABLED"] == "true")
   PostHog::Client.new(
     api_key: ENV.fetch("POSTHOG_API_KEY"),
     host: ENV.fetch("POSTHOG_HOST", "https://us.i.posthog.com"),
